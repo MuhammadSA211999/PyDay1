cc18c8ecca5150c1074aee217813a66f3d8b86eb
Muhammad SA <MuhammadSA211999>
2023-07-21 22:17:09 +0600

    PyDay1

---
+++ commentAndEscapeSeq.py
@@ -0,0 +1,9 @@
+# comment and escape sequences
+name = 'msa'
+comName = 'Tarmat Inc'
+'''
+MSA is a Bangladeshi edTech and Social etreprenuer. He found Tarmat Inc in 2024.He hardly try to estiblish womens proper rights in everywhere.
+'''
+mobileNum = '01322697553'
+print(mobileNum)
+# backSlash ke pythone escape sequence bola hoy.
