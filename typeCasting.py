f683be2aaf4ef3e4b673641fa76ca41a5b565e15
Muhammad SA <MuhammadSA211999>
2023-07-22 00:05:35 +0600

    typeCasting

---
+++ typeCasting.py
@@ -0,0 +1,13 @@
+# typeCasting: convert the data type into another data type. like string to integer or float.
+
+# **Built in input function diye shudumatro **string neya zay. pore onno data type convert kore kaj korte hoy
+
+num1 = float(input('Enter the first number:'))
+num2 = float(input('Enter the second number:'))
+# user give string value, then convert it float number
+
+# result = float(num1+num2)
+result = int(num1+num2)
+
+# here we float number => integer number
+print(result)
