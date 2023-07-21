0c6c29f0e292dd2cb63e5c86be110e086ecd1efa
Muhammad SA <MuhammadSA211999>
2023-07-21 23:18:59 +0600

    numerical opertors

---
+++ numericOperators.py
@@ -0,0 +1,26 @@
+'''
++ for plus, make one 
+- for minus, make one
+/ divided make one fractional or integer number
+* multiply make one
+% modulas = vag shesh janar jonno modulas kora hoy
+// floor = kuno vognangsher porno songkha pawar jonno floor bebohar kori
+** exponentiation = kuno songkhar power deyar jonno  exponentiation deya hoy
+
+'''
+
+num1 = 20
+num2 = 3
+
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+
+# complex
+print(num1 % num2)
+# 2
+print(num1//num2)
+# 6
+print(num1**num2)
+# 8000
