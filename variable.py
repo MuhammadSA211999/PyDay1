0e9c1b938503a6f2da035e19a318ad868f286720
Muhammad SA <MuhammadSA211999>
2023-07-21 22:33:35 +0600

    variable

---
+++ variable.py
@@ -0,0 +1,15 @@
+name = 'Muhammad Saleh Ahmad'
+name2 = 'MSA'
+age = 24
+resultCGPA = 3.78
+
+print('His anme is ' + name + " but he is well known as "+name2)
+
+print('He is', age, 'years old')
+print('He gain', resultCGPA, 'total in exam')
+
+'''
+NOTE:
+**ekt string er sathe arekti string + diye concat kora zay.
+kintu string er sathe number ba number er sathe string , diye concat korte hoy.**
+'''
