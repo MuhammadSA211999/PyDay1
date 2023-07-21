6b6913614c370598cfc3675f0c6fb1a8475021c2
Muhammad SA <MuhammadSA211999>
2023-07-21 23:45:43 +0600

    get user data by input function

---
+++ getInput.py
@@ -0,0 +1,12 @@
+# python e user er kach theke data neyar jonno built in *input* function bebohar kora hoy. input function e placeholder bebohar kora zay. input er data variable kora zay
+
+# get input from users
+name = input("Enter your name: ")
+# protom input (Enter your name) er data *name* variable e store kora hoyeche
+age = input("Enter your age: ")
+company = input("Enter your company name: ")
+
+# print users information by print function
+print('Name is '+name)
+print('He is', age, 'years old')
+print(name+' owns '+company)
